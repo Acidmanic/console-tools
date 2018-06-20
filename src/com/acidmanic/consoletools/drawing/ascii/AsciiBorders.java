117f39f3aa8319845f4d18211426ab216f5e343b
Mani Moayedi <Acidmanic.moayedi@gmail.com>
2018-06-20 16:45:59 +0430

    Add some premade Ascii borders

---
+++ src/com/acidmanic/consoletools/drawing/ascii/AsciiBorders.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.acidmanic.consoletools.drawing.ascii;
+
+/**
+ *
+ * @author Mani Moayedi (acidmanic.moayedi@gmail.com)
+ */
+public final class AsciiBorders {
+
+    public static final AsciiBorder NONE = new AsciiBorder(" ", " ", " ", " ", " ", " ",
+            " ", " ", " ", " ", " ", " ", " ");
+    public static final AsciiBorder SOLID = new AsciiBorder("─", "│", "─", "│",
+            "┌", "┐", "└", "┘", "├", "┤", "┬", "┴", "┼");
+}
