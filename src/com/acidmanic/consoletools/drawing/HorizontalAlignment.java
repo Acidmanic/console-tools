478125f5d7b417b4cf6a144c3d12fec60e149fcd
Mani Moayedi <Acidmanic.moayedi@gmail.com>
2018-06-22 13:08:12 +0430

    Add textAligner content modifier

---
+++ src/com/acidmanic/consoletools/drawing/HorizontalAlignment.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.acidmanic.consoletools.drawing;
+
+/**
+ *
+ * @author Mani Moayedi (acidmanic.moayedi@gmail.com)
+ */
+public enum HorizontalAlignment {
+    Left,
+    Center,
+    Right
+}
