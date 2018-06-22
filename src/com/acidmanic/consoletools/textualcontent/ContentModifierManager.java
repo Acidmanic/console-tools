f0c1cc4471e205c71744d352ec57a0084b52e41a
Mani Moayedi <Acidmanic.moayedi@gmail.com>
2018-06-22 12:53:02 +0430

    Use cons-injectable ContentModifierPriority for sorting contentModifiers

--- src/com/acidmanic/consoletools/textualcontent/ContentModifierManager.java
+++ src/com/acidmanic/consoletools/textualcontent/ContentModifierManager.java
@@ -5,6 +5,7 @@
  */
 package com.acidmanic.consoletools.textualcontent;
 
+import com.acidmanic.consoletools.textualcontent.builtin.BuiltinContentModifierPriority;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -16,8 +17,14 @@ public class ContentModifierManager {
 
     private final HashMap<String, Integer> modifersIndexes;
     private final ArrayList<ContentModifier> modifiers;
+    private final ContentModifierPriority priority;
 
     public ContentModifierManager() {
+        this(new BuiltinContentModifierPriority());
+    }
+
+    public ContentModifierManager(ContentModifierPriority priority) {
+        this.priority = priority;
         this.modifersIndexes = new HashMap<>();
         this.modifiers = new ArrayList<>();
     }
@@ -38,20 +45,49 @@ public class ContentModifierManager {
             int index = this.modifersIndexes.get(key);
             this.modifiers.remove(index);
             this.modifersIndexes.remove(key);
-            
+
         }
     }
 
-    public void removeModifier(Class type){
+    public void removeModifier(Class type) {
         removeModifierIfExists(type.getName());
     }
-    
+
     public Content applyAll(Content content) {
-        for(ContentModifier modifier:this.modifiers){
+        sortByPriority();
+        for (ContentModifier modifier : this.modifiers) {
             modifier.setInnerContent(content);
             content = modifier;
         }
         return content;
     }
 
+    private void sortByPriority() {
+        boolean isSorted = false;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < this.modifiers.size() - 1; i++) {
+                if (this.priority.compare(this.modifiers.get(i),
+                        this.modifiers.get(i + 1))) {
+                    isSorted = false;
+                    swapModifiers(i, i + 1);
+                }
+            }
+        }
+    }
+
+    private void swapModifiers(int i1, int i2) {
+        String keyWrapped = this.modifiers.get(i1).getClass().getName();
+        String keyWrapper = this.modifiers.get(i2).getClass().getName();
+        this.modifersIndexes.remove(keyWrapped);
+        this.modifersIndexes.remove(keyWrapper);
+        this.modifersIndexes.put(keyWrapper, i1);
+        this.modifersIndexes.put(keyWrapped, i2);
+        ContentModifier temp = this.modifiers.get(i1);
+        this.modifiers.set(i1, this.modifiers.get(i2));
+        this.modifiers.set(i2, temp);
+    }
+
+    
+    
 }
