1e657eccf68e380c0656742f1a0a400c17f48a9f
Mani <goj_loj@yahoo.com>
2018-06-28 22:53:54 +0430

    Add TerminalControlEscapeSequences

---
+++ test/com/acidmanic/consoletools/drawing/interaction/TerminalControlEscapeSequencesTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.acidmanic.consoletools.drawing.interaction;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Mani Moayedi (acidmanic.moayedi@gmail.com)
+ */
+public class TerminalControlEscapeSequencesTest {
+    
+    public TerminalControlEscapeSequencesTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        fail("The test case is a prototype.");
+    }
+    
+}
