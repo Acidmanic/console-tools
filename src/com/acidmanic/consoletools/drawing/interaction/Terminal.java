0e0a568dcd0008a3b3b4653919ab80f203a08428
Mani <goj_loj@yahoo.com>
2018-06-30 11:43:24 +0430

    Add setScreenAttributes

--- src/com/acidmanic/consoletools/drawing/interaction/Terminal.java
+++ src/com/acidmanic/consoletools/drawing/interaction/Terminal.java
@@ -6,6 +6,10 @@
 package com.acidmanic.consoletools.drawing.interaction;
 
 import com.acidmanic.consoletools.drawing.Size;
+import static com.acidmanic.consoletools.drawing.interaction.TerminalControlEscapeSequences.*;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -14,28 +18,55 @@ import com.acidmanic.consoletools.drawing.Size;
 public class Terminal {
 
     private static final String ESCAPE_S = String.format("%s", TerminalControlEscapeSequences.ESCAPE);
-    
+
     public Size queryCursorPosition() {
         String command = String.format("%s[6n", TerminalControlEscapeSequences.ESCAPE);
         try {
             System.out.write(command.getBytes());
-            if (System.console()!=null){
+            if (System.console() != null) {
                 char[] chars = System.console().readPassword();
-            String response = new String(chars);
-            response=response.replace(ESCAPE_S, "");
-            response=response.replace("[", "");
-            response=response.replace("R", "");
-            String[] rc = response.split(";");
-            int r = Integer.parseInt(rc[0]);
-            int c = Integer.parseInt(rc[1]);
-            return new Size(r, c);
+                String response = new String(chars);
+                response = response.replace(ESCAPE_S, "");
+                response = response.replace("[", "");
+                response = response.replace("R", "");
+                String[] rc = response.split(";");
+                int r = Integer.parseInt(rc[0]);
+                int c = Integer.parseInt(rc[1]);
+                return new Size(r, c);
             }
         } catch (Throwable ex) {
 
         }
         return new Size();
     }
+
+    public void setScreenAttributes(int foreground, int background) {
+        String command = String.format("%s[%d;%dm", ESCAPE, foreground, background);
+        executeOnTerminal(command);
+    }
     
-    
-    
+    public void setScreenAttributes(TerminalStyle style){
+        String command = createCommand(style);
+        executeOnTerminal(command);
+    }
+
+    private void executeOnTerminal(String command) {
+        try {
+            System.out.write(command.getBytes());
+        } catch (Throwable ex) {
+        }
+    }
+
+    private String createCommand(TerminalStyle style) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(ESCAPE).append("[");
+        String sep = "";
+        for (int att : style.getAll()) {
+            sb.append(sep).append(att);
+            sep = ";";
+        }
+        sb.append("m");
+        return sb.toString();
+    }
+
 }
---
+++ src/com/acidmanic/consoletools/drawing/interaction/TerminalStyle.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.acidmanic.consoletools.drawing.interaction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Mani Moayedi (acidmanic.moayedi@gmail.com)
+ */
+public class TerminalStyle {
+
+    private int foregroundColor = TerminalControlEscapeSequences.FOREGROUND_WHITE;
+    private int backgroundColor = TerminalControlEscapeSequences.BACKGROUND_BLACK;
+    private final List<Integer> properties;
+
+    public TerminalStyle() {
+        this.properties = new ArrayList<>();
+        this.properties.add(TerminalControlEscapeSequences.ATTR_RESET_ALL);
+    }
+
+    public TerminalStyle(int foreground, int background) {
+        this();
+        this.foregroundColor = foreground;
+        this.backgroundColor = background;
+    }
+
+    public int getForegroundColor() {
+        return foregroundColor;
+    }
+
+    public void setForegroundColor(int foregroundColor) {
+        this.foregroundColor = foregroundColor;
+    }
+
+    public int getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    public void setBackgroundColor(int backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
+
+    public void addAttributes(int attribute) {
+        this.properties.add(attribute);
+    }
+    
+    public List<Integer> getAll(){
+        ArrayList<Integer> ret = new ArrayList<>(this.properties);
+        ret.add(foregroundColor);
+        ret.add(backgroundColor);
+        return ret;
+    }
+
+}
