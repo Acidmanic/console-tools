e9ae669755cffb1439e03f923a49b9c84e9cf2e4
Mani <goj_loj@yahoo.com>
2018-07-09 16:05:20 +0430

    Move playgrounds back to src to be testable by jar file

--- src/consoletools/tests/playgrounds/TerminalBackwardTest.java
+++ src/consoletools/tests/playgrounds/TerminalBackwardTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package console.tools.tests.playgrounds;
+package consoletools.tests.playgrounds;
 
 import com.acidmanic.consoletools.terminal.Terminal;
 import com.acidmanic.consoletools.terminal.TerminalCommandBuilder;
