579b2bf64309ef0823730be67970f525bd0edc30
Mani <goj_loj@yahoo.com>
2018-10-21 09:00:05 +0330

    Test Decorators

---
+++ test/com/acidmanic/consoletools/playgrounds/DecoratorsTest.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.acidmanic.consoletools.playgrounds;
+
+import com.acidmanic.consoletools.drawing.Padding;
+import com.acidmanic.consoletools.drawing.Size;
+import com.acidmanic.consoletools.drawing.ascii.AsciiBorders;
+import com.acidmanic.consoletools.rendering.BufferedStringRenderingContext;
+import com.acidmanic.consoletools.rendering.Renderable;
+import com.acidmanic.consoletools.rendering.decorators.Bordered;
+import com.acidmanic.consoletools.rendering.decorators.Padded;
+import com.acidmanic.consoletools.table.Cell;
+import com.acidmanic.consoletools.terminal.Terminal;
+import com.acidmanic.consoletools.terminal.styling.TerminalStyles;
+
+/**
+ *
+ * @author Mani Moayedi (acidmanic.moayedi@gmail.com)
+ */
+public class DecoratorsTest {
+
+    public static void main(String[] args) {
+
+        Cell c = new Cell("CELL");
+
+        print(c, "Original");
+
+        Renderable padded = new Padded(c, new Padding(3));
+
+        print(padded, "Padded");
+
+        Renderable bordered = new Bordered(padded, AsciiBorders.SOLID);
+
+        print(bordered, "Bordered");
+
+        Renderable margined = new Padded(bordered, new Padding(3));
+
+        print(margined, "Magined");
+
+        Renderable outlined = new Bordered(margined, AsciiBorders.DOUBLELINE);
+
+        print(outlined, "Outlined");
+
+    }
+
+    private static void print(Renderable renderable, String message) {
+        System.out.println("-----------------------");
+        System.out.println(message);
+        System.out.println("-----------------------");
+
+        Size size = renderable.measure();
+
+        BufferedStringRenderingContext context = new BufferedStringRenderingContext(size);
+
+        context.clear();
+
+        renderable.render(context);
+
+        String result = context.represent();
+
+        new Terminal().setScreenAttributes(TerminalStyles.UglyMac);
+        System.out.println(result);
+        new Terminal().resetScreenAttributes();
+    }
+
+}
