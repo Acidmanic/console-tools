d3d094306ab91ecd8f1ec5fe27547bca43bae9b9
Mani <goj_loj@yahoo.com>
2018-10-21 19:45:15 +0330

    Correct PaddindBottomPlayground test file

--- src/consoletools/tests/playgrounds/PaddingBottomPlayground.java
+++ src/consoletools/tests/playgrounds/PaddingBottomPlayground.java
@@ -6,10 +6,7 @@
 package consoletools.tests.playgrounds;
 
 import com.acidmanic.consoletools.drawing.Padding;
-import com.acidmanic.consoletools.drawing.ascii.AsciiBorder;
 import com.acidmanic.consoletools.drawing.ascii.AsciiBorders;
-import com.acidmanic.consoletools.rendering.Renderable;
-import com.acidmanic.consoletools.rendering.decorators.Bordered;
 import com.acidmanic.consoletools.table.Cell;
 import com.acidmanic.consoletools.table.Row;
 import com.acidmanic.consoletools.table.Table;
@@ -18,7 +15,7 @@ import com.acidmanic.consoletools.table.Table;
  *
  * @author Mani Moayedi (acidmanic.moayedi@gmail.com)
  */
-public class PaddingButtomPlayground {
+public class PaddingBottomPlayground {
 
     public static void main(String[] args) {
         Table table = new Table();
@@ -27,7 +24,9 @@ public class PaddingButtomPlayground {
 
         Cell cell2 = new Cell("CELL\nCONTENT");
 
-        row2.getCells().add(new Bordered(cell2,AsciiBorders.SOLID));
+        cell2.setBorder(AsciiBorders.SOLID);
+
+        row2.getCells().add(cell2);
 
         cell2.setPadding(new Padding(4, 4, 4, 4));
 
