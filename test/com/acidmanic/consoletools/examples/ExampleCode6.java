1a96082b3205f4df1a8c3268cc70cc01ce407196
Mani <goj_loj@yahoo.com>
2018-10-23 09:33:19 +0330

    Add Testcode for TableBuilder

---
+++ test/com/acidmanic/consoletools/examples/ExampleCode6.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.acidmanic.consoletools.examples;
+
+import com.acidmanic.consoletools.drawing.AsciiBorder;
+import com.acidmanic.consoletools.drawing.AsciiBorders;
+import com.acidmanic.consoletools.rendering.BufferedStringRenderingContext;
+import com.acidmanic.consoletools.table.Table;
+import com.acidmanic.consoletools.table.builders.TableBuilder;
+
+/**
+ *
+ * @author Mani Moayedi (acidmanic.moayedi@gmail.com)
+ */
+public class ExampleCode6 {
+
+    public static void main(String[] args) {
+
+        Table table = new TableBuilder()
+                .tableBorder(AsciiBorders.BOLD)
+                .cell("OnlyCell")
+                .border()
+                .cell((TableBuilder builder) -> builder
+                        .row().cell("00").cell("01")
+                        .row().cell("10").cell("11")
+                        .borderAll().tableBorder())
+                .row().cell("Bottom-Left").border()
+                .cell("Middle").border()
+                .cell("Bottom-Right").border()
+                .row().cell((TableBuilder builder) -> builder.table(3, 3)
+                        .textAll("*").borderAll().padAll(3,1).tableBorder())
+                .build();
+
+        BufferedStringRenderingContext context = new BufferedStringRenderingContext(table.measure());
+        context.clear();
+        table.render(context);
+        System.out.println(context.represent());
+
+    }
+}
